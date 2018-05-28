d9437d01e0a1f0a4314855760cd0d5f63c0f3bf4
garry pritchard <GarryPritchard28@Hotmail.com>
2018-05-28 22:03:25 +0100

    web api controller

---
+++ Checkout/Controllers/CheckoutController.cs
@@ -0,0 +1,15 @@
+﻿namespace Checkout.Controllers
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+
+    public class CheckoutController : ApiController
+    {
+        public async Task<decimal> CalculateBasket(List<string> products)
+        {
+            var checkoutEngine = new CheckoutEngine();
+            return await Task.FromResult(checkoutEngine.CalculateBasket(products));
+        }
+    }
+}
